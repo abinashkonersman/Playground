e934abed2761aa04d5e4c09d8c2045b9d0e05f46
abinash biswal Konersman <dougabi@yahoo.com>
2020-03-16 13:07:39 -0500

    view controller

--- Playground/ViewController.swift
+++ Playground/ViewController.swift
@@ -14,13 +14,17 @@ class ViewController: UIViewController {
     @IBAction func touchCard(_ sender: UIButton) {
        flipCard(withEmoji: "👻", on: sender)
     }
+    @IBAction func touchCard2(_ sender: UIButton) {
+        flipCard(withEmoji: "👹", on: sender)
+        
+    }
     func flipCard(withEmoji emoji : String, on button: UIButton){
         if button.currentTitle == emoji{
-            button.setTitle("", for:UIControlState.normal)
-            button.backgroundColor= #colorLiteral(red: 1, green: 0.5357117653, blue: 0, alpha: 1)
+            button.setTitle("", for:UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         } else{
-            button.setTitle(emoji, for: UIControlState.normal)
-            button.backgroundColor=
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
         
     }
